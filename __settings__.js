44b08ccf1c245c9adde3b9b43c39a0c8802076b9
CipherWeb <78916202+cipherwebllc@users.noreply.github.com>
2022-09-20 00:18:48 +0900

    upload

---
+++ __settings__.js
@@ -0,0 +1,23 @@
+ASSET_PREFIX = "";
+SCRIPT_PREFIX = "";
+SCENE_PATH = "1535446.json";
+CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': true,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "default"
+};
+SCRIPTS = [ 102565343, 102565351, 102565353, 102565367, 102565370 ];
+CONFIG_FILENAME = "config.json";
+INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: true,
+    useTouch: true
+};
+pc.script.legacy = false;
+PRELOAD_MODULES = [
+    {'moduleName' : 'BASIS', 'glueUrl' : 'files/assets/102565340/1/basis.wasm.js', 'wasmUrl' : 'files/assets/102565341/1/basis.wasm.wasm', 'fallbackUrl' : 'files/assets/102565339/1/basis.js', 'preload' : false},
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/102565336/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/102565337/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/102565335/1/ammo.js', 'preload' : true},
+];
